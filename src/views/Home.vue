a1c3009f5174056c04ae5c6d73bdbe2dd95aaed6
GODU-LZR <2980560533@qq.com>
2025-01-13 04:59:15 +0800

    qiankun vue vue-route vue-x element-ui 图表组件 qiankun基座微应用框架整合

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<!-- vue-qk-assistant/src/views/Home.vue -->
+<template>
+  <div>
+    <h2>智能助理模块首页</h2>
+    <el-button type="primary" @click="showMessage">点击测试</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostsHome',
+  methods: {
+    showMessage() {
+      this.$message('智能助理子应用的 ElementUI 按钮点击了！');
+    }
+  }
+}
+</script>
